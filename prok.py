8d73f94369eb0609a72f541485074f707da864fb
Курочкин Афанасий <svistov.mm@phystech.edu>
2023-11-09 13:37:43 +0300

    Чо блин

--- prok.py
+++ prok.py
@@ -1,12 +1,14 @@
 import random
 def death_check():
+  global game_over
   if health<=0:
     game_over=True
 print('test')
 health=100
 mana=20
-game_over=True
-while game_over:
+game_over=False
+while not game_over:
   health-=random.random()
+  print(health)
   death_check()
 print('game_over')
