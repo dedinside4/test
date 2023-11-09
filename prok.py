b643095dbab2c580acacb38b119074a3d4a08411
dedinside4 <149906377+dedinside4@users.noreply.github.com>
2023-11-09 13:32:37 +0300

    Написал мини игру

--- prok.py
+++ prok.py
@@ -1,1 +1,12 @@
+import random
+def death_check():
+  if health<=0:
+    game_over=True
 print('test')
+health=100
+mana=20
+game_over=True
+while game_over:
+  health-=random.random()
+  death_check()
+print('game_over')
